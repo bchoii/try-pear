0396646414784ad963cf0e78cc9ac0c9ffc8fcd2
Bernard Choi <bchoii@gmail.com>
2024-07-02 09:49:57 +0800

    find selfId

--- ping-app/index.js
+++ ping-app/index.js
@@ -7,27 +7,27 @@ const swarm = new Hyperswarm();
 Pear.teardown(() => swarm.destroy());
 
 // Keep track of all connections and console.log incoming data
-const conns = [];
 swarm.on("connection", (conn) => {
+  // console.log("conn", conn);
+  // console.log("remotePublicKey", b4a.toString(conn.remotePublicKey, "hex"));
+  // console.log("publicKey", b4a.toString(conn.publicKey, "hex"));
+
   const name = b4a.toString(conn.remotePublicKey, "hex");
   console.log("* got a connection from:", name, "*");
-  conns.push(conn);
-  console.log(`${conns.length} connections now.`);
-  conn.once("close", () => {
-    console.log("Connection Closed.");
-    conns.splice(conns.indexOf(conn), 1);
-  });
+  console.log(`${[...swarm.connections].length} connections now.`);
   conn.on("data", (data) =>
     console.log(`received [${data}] from [${name.slice(0, 5)}]`)
   );
   conn.on("error", (e) => console.log(`Connection error: ${e}`));
+  conn.once("close", () => {
+    console.log("Connection Closed.");
+  });
 });
 
 setInterval(() => {
-  console.log(`Sending PING to ${conns.length} connections`);
-  for (const conn of conns) {
-    conn.write(`PING`);
-  }
+  const peers = [...swarm.connections];
+  console.log(`Sending PING to ${peers.length} connections`);
+  for (const peer of peers) peer.write("PING");
 }, 10000);
 
 // Join a common topic
@@ -37,6 +37,12 @@ const topic = process.argv[3]
   : crypto.randomBytes(32);
 
 const discovery = swarm.join(topic, { client: true, server: true });
+// console.log("discovery", discovery);
+console.log("swarm", swarm);
+console.log(
+  "swarm.keyPair.publicKey",
+  b4a.toString(swarm.keyPair.publicKey, "hex")
+);
 
 // The flushed promise will resolve when the topic has been fully announced to the DHT
 discovery.flushed().then(() => {
